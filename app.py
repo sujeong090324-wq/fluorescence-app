5fa00bf8c28f31b803b2d8f5da1fb9866c708b20
sujeong090324-wq <sujeong090324@gmail.com>
2026-09-04 01:03:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from PIL import Image, ImageOps
+from PIL import Image, ImageOps, ImageDraw
 import plotly.express as px
 import os
 
